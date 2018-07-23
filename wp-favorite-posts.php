a3d6f615bcdb08d3d301eea066ca2dcda4026ad6
Simon Doppler <simon.doppler@gmx.at>
2018-07-23 14:52:44 +0200

    check if user meta is an array
    experienced problems if there was no meta data stored yet

--- wp-favorite-posts.php
+++ wp-favorite-posts.php
@@ -150,6 +150,9 @@ function wpfp_die_or_go($str) {
 
 function wpfp_add_to_usermeta($post_id) {
     $wpfp_favorites = wpfp_get_user_meta();
+    if ( !is_array( $wpfp_favorites ) ) {
+        $wpfp_favorites = array();
+    }
     $wpfp_favorites[] = $post_id;
     wpfp_update_user_meta($wpfp_favorites);
     return true;
